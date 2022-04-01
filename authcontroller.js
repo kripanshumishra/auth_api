45208a2d85ac68ed4600e8e7bc7fc50b74e88ab2
kripanshu mishra <mishrakripanshu303@gmail.com>
2022-04-01 19:45:26 +0530

    gh

--- authcontroller.js
+++ authcontroller.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const User = require('../models/userschema');
+const User = require('./models/userschema');
 
 
 
