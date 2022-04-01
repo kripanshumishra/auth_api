0e9d4fb401d3e29b70af8dcf303f4a3458f4aecc
kripanshu mishra <mishrakripanshu303@gmail.com>
2022-04-01 17:11:40 +0530

    first

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const connection = require('./util/database');
+require('dotenv').config()
+const port = process.env.PORT || 4000;
+const cors = require('cors');
+app.use(cors())
+
+const AuthController = require('./controller/authController');
+app.use('/api/auth', AuthController);
+// app.get('/fake',(req,res)=>res.status(200).json({ msg : 'worked'}));
+
+app.listen(port,() => {
+    console.log(`Running on port ${port}`)
+})
